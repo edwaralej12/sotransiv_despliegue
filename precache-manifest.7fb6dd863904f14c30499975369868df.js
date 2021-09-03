6bf694e2a36901f80ff0d5d83519286acad30ad7
edwaralej12 <edwaralejandro595@gmail.com>
2021-09-02 20:43:45 -0500

    Updates

---
+++ precache-manifest.7fb6dd863904f14c30499975369868df.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e8d30b242d226581efbad67e613fff",
+    "url": "/sotransiv_despliegue/index.html"
+  },
+  {
+    "revision": "bc7d180477e89ae1235d",
+    "url": "/sotransiv_despliegue/static/css/2.b8a3b08a.chunk.css"
+  },
+  {
+    "revision": "ad6ab4ed0857888ca7e2",
+    "url": "/sotransiv_despliegue/static/css/main.c7321cae.chunk.css"
+  },
+  {
+    "revision": "bc7d180477e89ae1235d",
+    "url": "/sotransiv_despliegue/static/js/2.6eda6328.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/sotransiv_despliegue/static/js/2.6eda6328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6ab4ed0857888ca7e2",
+    "url": "/sotransiv_despliegue/static/js/main.a9db91d5.chunk.js"
+  },
+  {
+    "revision": "3fe07609cd97fd243a0d",
+    "url": "/sotransiv_despliegue/static/js/runtime-main.4296290c.js"
+  },
+  {
+    "revision": "cc681753dc336ed737b7837581a7b132",
+    "url": "/sotransiv_despliegue/static/media/LogoWhite.cc681753.png"
+  },
+  {
+    "revision": "20537f478b26ea2b975055de4cf4ad06",
+    "url": "/sotransiv_despliegue/static/media/Logo_color.20537f47.png"
+  },
+  {
+    "revision": "7b2695a987319c57f90825178746ff80",
+    "url": "/sotransiv_despliegue/static/media/fondo.7b2695a9.jpg"
+  },
+  {
+    "revision": "a23ad24672caf67cd49cbf02fb9b912c",
+    "url": "/sotransiv_despliegue/static/media/icono.a23ad246.png"
+  },
+  {
+    "revision": "daebef92ccd486ea33cedd0abe3134b6",
+    "url": "/sotransiv_despliegue/static/media/user.daebef92.jpg"
+  }
+]);
